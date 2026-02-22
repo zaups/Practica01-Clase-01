3314da986587ff3b70ef4de6383eb4075753deca
zaups <adolecenge25fosforecente07@gmail.com>
2026-02-21 21:36:08 -0600

    Add files via upload
    Practica 01 Clase 01, 21 de febrero 2026

---
+++ Bienvenido1 Practica 01 Clase01/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	
+	cout <<"************************"<< endl;
+	cout <<"*                      *"<< endl;
+	cout <<"*     Bienvenido       *"<< endl;
+	cout <<"*                      *"<< endl;
+	cout <<"************************"<< endl;
+	
+	
+	return 0;
+}
+
---
+++ Bienvenido2 Practica01 Clase01/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	
+	cout <<"========================"<< endl;
+	cout <<"||                    ||"<< endl;
+	cout <<"||     Bienvenido     ||"<< endl;
+	cout <<"||                    ||"<< endl;
+	cout <<"========================"<< endl;
+	
+	return 0;
+}
+
